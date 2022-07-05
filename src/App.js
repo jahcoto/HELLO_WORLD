47ad8ea36b0e87c7cead33bb7dab682c763c75a6
jahcoto <jahcoto@gmail.com>
2022-07-05 09:20:24 -0700

    Commit: Se agrego componente Hello_World

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import Hello_world from './components/Hello_world.js';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+      <Hello_world />
     </div>
   );
 }
---
+++ src/components/Hello_world.js
@@ -0,0 +1,7 @@
+const Hello_world = () => {
+    return ( 
+        <h1>Hola Mundo</h1>
+     );
+}
+ 
+export default Hello_world;
